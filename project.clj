16afb72eb96aa877dec671b519ef8fbb6aca1835
tsuhovenko <tsuhovenko@cogniance.com>
2015-11-07 22:08:52 +0200

    added html table parser

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
   				 [incanter "1.5.6"]
-  				 [enlive "1.1.6"]])
+  				 [enlive "1.1.6"]]
+  :main getting-data.core)
--- src/getting_data/core.clj
+++ src/getting_data/core.clj
@@ -1,14 +1,32 @@
-(ns getting-data.core)
+; REPL: (require `ch1.importing :verbose :reload)
+; (load-data (str "http://www.ericrochester.com/" "clj-data-analysis/data/small-sample-table.html"))
 
-(use 'incanter.core
-	 'incanter.io)
+(ns getting-data.core
 
-(deft read_csv
-	(read-dataset "data/data.csv"))
+(:require
+  [clojure.string :as string]
+  [net.cgrand.enlive-html :as html]
+  [incanter.core :as i])
+(:import [java.net URL]))
 
 
+(defn to-keyword
+  [input]
+  (-> input string/lower-case (string/replace \space \-) keyword))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn load-data
+  [url]
+  (let [page (html/html-resource (URL. url))
+        table (html/select page [:table#data])
+        headers (->>
+                  (html/select table [:tr :th])
+                  (map html/text)
+                  (map to-keyword)
+                  vec)
+        rows (->> (html/select table [:tr])
+                  (map #(html/select % [:td]))
+                  (map #(map html/text %))
+                  (filter seq))]
+    (i/dataset headers rows)))
+
+;(defn -main (println "hello"))
