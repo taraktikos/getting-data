186299693c88a2bbcaba38d290453f988b86a4b4
tsuhovenko <tsuhovenko@cogniance.com>
2015-11-09 10:14:17 +0200

    parse list

--- src/getting_data/core.clj
+++ src/getting_data/core.clj
@@ -1,5 +1,7 @@
-; REPL: (require `ch1.importing :verbose :reload)
+; REPL:
+; (require `getting-data.core :verbose :reload)
 ; (load-data (str "http://www.ericrochester.com/" "clj-data-analysis/data/small-sample-table.html"))
+; (load-data2 (str "http://www.ericrochester.com/" "clj-data-analysis/data/small-sample-list.html"))
 
 (ns getting-data.core
 
@@ -28,5 +30,30 @@
                   (map #(map html/text %))
                   (filter seq))]
     (i/dataset headers rows)))
+
+
+(defn get-family
+  [article]
+  (string/join
+    (map html/text (html/select article [:header :h2]))))
+
+(defn get-person
+  [li]
+  (let [[{pnames :content} rel] (:content li)]
+    {:name (apply str pnames)
+     :relationship (string/trim rel)}))
+
+(defn get-rows
+  [article]
+  (let [family (get-family article)]
+    (map #(assoc % :family family)
+         (map get-person
+              (html/select article [:ul :li])))))
+
+(defn load-data2
+  [html-url]
+  (let [html (html/html-resource (URL. html-url))
+        articles (html/select html [:articles])]
+    (i/to-dataset (mapcat get-rows articles))))
 
 ;(defn -main (println "hello"))
